8494b530a4e657b4593485386874767b39de70e4
Jose Duarte <jdanny0115@gmail.com>
2026-08-05 17:53:57 -0400

    Fix comments and improve readability in main.c

--- main.c
+++ main.c
@@ -1,30 +1,43 @@
+/****************************************************************************
+ *FILENAME:     main.c
+ *DESCRIPTION:  It's a simple version of Conway's Game of Life written in C.
+ *CREATOR:      Jose Duarte
+ *DATE:         August 5, 2026
+ *LICENSE:      GPL-3.0 license
+ ****************************************************************************/
 #include <stdio.h>  //The Standard Input Output header file
 #include <stdlib.h> //The Standard Library header file - Useful utilities
-#include <time.h>
-#include <stdbool.h>
+#include <time.h>   //Needed for making our game loop
+#include <stdbool.h>//This is the Boolean header file
 
-#define MAXROW 5  //The max the grid can be in height is 5 rows.
-#define MAXCOL 5  //The max the grid can be in width is 5 columns.
+//The grid is going to be 5x5 cells
+#define MAXROW 5
+#define MAXCOL 5
 
-#define FPS 1
+//2 frames per second - I left it slow instead of 60 FPS since we want to see these changes
+#define FPS 2
+//We want to know how long each frame is in milliseconds so we can know when to change frame
 #define FRAME_LENGTH (1000.0/FPS) //1000.0 milliseconds divided by 60 FPS = 16.66 milliseconds
 
+//I have this so when the grid is blank, it will stop running
 bool running = true;
 
 //Notes:
 //This is how the grid is going to work.
-//We will be using a 2D arrary, where each cell can be DEAD or ALIVE
+//We will be using a 2D array, where each cell can be DEAD or ALIVE
 //.....     [0,0][0,1][0,2][0,3][0,4]  
-//.***.     [1,0][1,1][1,2][1,3][1,4]
+//.....     [1,0][1,1][1,2][1,3][1,4]
 //.***. --> [2,0][2,1][2,2][2,3][2,4]
-//.***.     [3,0][3,1][3,2][3,3][3,4]
+//.....     [3,0][3,1][3,2][3,3][3,4]
 //.....     [4,0][4,1][4,2][4,3][4,4]
+//
+//
 //"The typedef keyword lets you create a new name (an alias) for an existing type." (w3school)
-//enum also called enumeration represents "a group of constants (unchangeable values)" (w3school)
+//enum, also called enumeration, represents "a group of constants (unchangeable values)" (w3school)
 
 
 
-//We are creating a type called "Cell" that is a enum which can be DEAD or ALIVE
+//We are creating a type called "Cell" that is an enum which can be DEAD or ALIVE
 typedef enum cell{
   DEAD,
   ALIVE
@@ -34,8 +47,9 @@ typedef enum cell{
 typedef Cell Grid[MAXROW][MAXCOL];
 
 
-
+//***************************************************************************
 //Here are the functions that we will use throughout this project.
+//***************************************************************************
 
 void copy_this_grid_to_that_grid(Grid this_grid, Grid that_grid){
 
@@ -48,7 +62,7 @@ void copy_this_grid_to_that_grid(Grid this_grid, Grid that_grid){
 }//end of copy_this_grid_to_that_grid function
 
 void make_grid_blank(Grid grid){
-  //Create a blank grid (DEAD cells)
+  //Create a blank grid of DEAD cells
   for(int row = 0; row < MAXROW; row++){
     for(int col = 0; col < MAXCOL; col++){
       grid[row][col] = DEAD;
@@ -78,7 +92,7 @@ void print_grid(Grid grid){
         printf(".");
       }
       else{
-        //If it is not, them display an asterisk.
+        //If it is not, then display an asterisk.
         printf("*");
       }
     }//end of column for loop
@@ -88,43 +102,53 @@ void print_grid(Grid grid){
 }//end of print_grid function
 
 void clear_screen(){
-  system("clear");  
+  //This will use the terminal command of "clear"
+  system("clear");
 }//end of clear_screen function
 
 //Here are the rules we will be checking
-//Underpopulation - Any alive cells will die if there are fewer than two neighbouring alive cells.
-bool check_for_underpopulation(int neighbour_num){
-  if(neighbour_num < 2){
+//Underpopulation - Any alive cells will die if there are fewer than two neighboring alive cells.
+bool check_for_underpopulation(int neighbor_num){
+  if(neighbor_num < 2){
     return true;
   }
   return false;
 }
 
-//Survival - Any alive cells will live if there are two or three neighbouring alive cells.
-bool check_for_survival(int neighbour_num){
-  if(neighbour_num == 2 || neighbour_num == 3){
+//Survival - Any alive cells will live if there are two or three neighboring alive cells.
+bool check_for_survival(int neighbor_num){
+  if(neighbor_num == 2 || neighbor_num == 3){
     return true;
   }
   return false;
 }
 
-//Overpopulation - Any alive cells will die if there are more than three neighbouring alive cells.
-bool check_for_overpopulation(int neighbour_num){
-  if(neighbour_num > 3){
+//Overpopulation - Any alive cells will die if there are more than three neighboring alive cells.
+bool check_for_overpopulation(int neighbor_num){
+  if(neighbor_num > 3){
     return true;
   }
   return false;
 }
 
-//Reproduction - Any dead cells will turn into an alive cell if there are exactly three neighbouring alive cells.
-bool check_for_reproduction(int neighbour_num){
-  if(neighbour_num == 3){
+//Reproduction - Any dead cells will turn into an alive cell if there are exactly three neighboring alive cells.
+bool check_for_reproduction(int neighbor_num){
+  if(neighbor_num == 3){
     return true;
   }
   return false;
 }
 
-int get_neighouring_alive_cell_num(Grid grid, int current_row, int current_col){
+//This will get your current cell's position and look around it if it can.
+//It is searching for ALIVE neighbors
+//# = Will check this Cell to see if it is ALIVE
+//O = Current cell
+//.....
+//.###.
+//.#O#.
+//.###.
+//.....
+int get_neighboring_alive_cell_num(Grid grid, int current_row, int current_col){
   int count = 0;
 
   for(int temp_row = current_row - 1; temp_row <= current_row + 1; temp_row++){
@@ -141,7 +165,7 @@ int get_neighouring_alive_cell_num(Grid grid, int current_row, int current_col){
   }//end of row for loop
   
   return count;
-}//end of get_neighouring_alive_cell_num function
+}//end of get_neighboring_alive_cell_num function
 
 void check_for_rules(Grid grid, Grid clone_grid){
   int alive = 0;
@@ -151,19 +175,19 @@ void check_for_rules(Grid grid, Grid clone_grid){
         alive++;
       }
 
-      int neighbour_num = get_neighouring_alive_cell_num(grid, row, col);
+      int neighbor_num = get_neighboring_alive_cell_num(grid, row, col);
 
       if(grid[row][col] == DEAD){
-        if(check_for_reproduction(neighbour_num)){
+        if(check_for_reproduction(neighbor_num)){
           clone_grid[row][col] = ALIVE;
         }
       }
       else{
         //Current cell is ALIVE
-        if(check_for_underpopulation(neighbour_num)||check_for_overpopulation(neighbour_num)){
+        if(check_for_underpopulation(neighbor_num)||check_for_overpopulation(neighbor_num)){
           clone_grid[row][col] = DEAD;
         }
-        else if(check_for_survival(neighbour_num)){
+        else if(check_for_survival(neighbor_num)){
           clone_grid[row][col] = ALIVE;
         }
       }//end of else statement
@@ -181,7 +205,7 @@ void check_for_rules(Grid grid, Grid clone_grid){
 
 
 void initialize(Grid grid, Grid clone){
-  //This initialize our grid by making it blank
+  //This initializes our grid by making it blank
   make_grid_blank(grid);
   make_grid_test(grid);
 
@@ -213,8 +237,8 @@ int main(){
 
   while(running){
     end_ticks = clock();
-    //Delta the time between the start and end tick in miliseconds
-    //delta = 1000 miliseconds * (clock tick length)/Clock ticks per second
+    //Delta the time between the start and end tick in milliseconds
+    //delta = 1000 milliseconds * (clock tick length)/Clock ticks per second
     delta = 1000 * (end_ticks - start_ticks)/CLOCKS_PER_SEC;
 
     if(delta >= FRAME_LENGTH){
